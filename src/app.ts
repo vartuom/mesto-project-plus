d1aebc1d9e0978bcfcab0d075a12cea60e7a16d2
vartuom <v.artuom@ya.ru>
2022-12-26 12:31:29 +0800

    26-12-home

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 const app = express();
 
