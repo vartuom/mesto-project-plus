55916e8ce893519ab4dff80e9ae684b193889b42
vartuom <v.artuom@ya.ru>
2022-12-27 12:32:16 +0800

    27-12-home

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import usersRouter from './routes/users'
 
 const { PORT = 3000 } = process.env;
+const app = express();
+app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-const app = express();
+app.use('/users', usersRouter);
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,8 +2,8 @@ import { Router } from 'express';
 import { createUser, getUsers, getUsersById } from '../controllers/user';
 
 const router = Router();
-router.get('/users', getUsers);
-router.get('/users/:userId', getUsersById);
-router.post('/users', createUser);
+router.get('/', getUsers);
+router.get('/:userId', getUsersById);
+router.post('/', createUser);
 
 export default router;
